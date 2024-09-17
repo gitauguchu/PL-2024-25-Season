0513b0285f72cdac0f1b036b78a6346d7e46ee6d
gitauguchu <gitaudennis59@gmail.com>
2024-09-17 11:35:20 +0300

    Changes to main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import os
 os.system('python push_to_blob.py')
 
 #Run the 'push_to_database' script using the system shell
-os.system('python push_to_database')
+os.system('python push_to_database.py')
